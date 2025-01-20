7030b3394a49610e64e319041ddbd324d6c19fd8
workingwithali <alirashid2020e@gmail.com>
2025-01-20 14:30:04 +0500

    category

--- models/e-commerce/category.models.js
+++ models/e-commerce/category.models.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-
+    name : {
+        type: String,
+        require : true
+    }
 },{timestamps:true})
 
 export const Category = mongoose.model('Category',categorySchema)
