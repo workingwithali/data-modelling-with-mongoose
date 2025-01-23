65512e962c21980272d87811f12571a26b5251c9
workingwithali <alirashid2020e@gmail.com>
2025-01-23 12:15:54 +0500

    doctor

--- models/hospital/doctor.models.js
+++ models/hospital/doctor.models.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({},{timestamps:true})
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        requried : true
+    },
+    salary : {
+        type : Number,
+        requried : true
+    },
+    qualification:[
+        {
+            type:String,
+            required : true
+        }
+    ],
+    experienceInYear:{
+        type: String,
+        required: true
+    },
+    workInHospitals:[
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            required : true
+        }
+    ]
+},{timestamps:true})
 
 export const Doctor = mongoose.model('Doctor',doctorSchema)
