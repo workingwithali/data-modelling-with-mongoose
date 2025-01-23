84d0ca6b80124ca4d54e0991133dde8eefabe19f
workingwithali <alirashid2020e@gmail.com>
2025-01-23 12:26:24 +0500

    zds

--- models/hospital/hospital.models.js
+++ models/hospital/hospital.models.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({},{timestamps:true})
+const hospitalSchema = new mongoose.Schema({
+    f
+},{timestamps:true})
 
 export const Hospital = mongoose.model('Hospital',hospitalSchema)
