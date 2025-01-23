d488b5ff8912cae4ebce3b7a6e9a29ef5524245c
workingwithali <alirashid2020e@gmail.com>
2025-01-23 12:09:09 +0500

    patient

--- models/hospital/patient.models.js
+++ models/hospital/patient.models.js
@@ -1,5 +1,36 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({},{timestamps:true})
+const patientSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        requried:true
+    },
+    diagonsedwith:{
+        type:String,
+        requried: true
+    },
+    address : {
+        type : String,
+        requried : true
+    },
+    age : {
+        type : String,
+        requried: true
+    },
+    bloodgroup: {
+        type: String,
+        requried
+    },
+    gender: {
+        type : String,
+        enum : ['m','f','o']
+    },
+    admittedIn:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        required : string        
+        }
+    ],
+},{timestamps:true})
 
 export const Patient = mongoose.model('Patient',patientSchema)
